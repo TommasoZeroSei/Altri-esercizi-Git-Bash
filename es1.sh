e85d2e612cd3fe789d50f6dc96be9afbd84ca573
TommasoZeroSei <panzeri.tommaso@studenti.salesianisesto.it>
2024-11-29 16:01:40 +0100

    C2: es1.sh added

--- es1.sh
+++ es1.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 cd $HOME/Desktop
+rm -rf branchRepoLog
+git init branchRepoLog
+cd branchRepoLog
 
-rm -rf mergeRepo
-git init mergeRepo
-cd mergeRepo
+contatore=1
+for i in {1..4}; do
+touch file$contatore.txt
+git add file$contatore.txt
+((contatore++))
+touch file$contatore.txt
+git add file$contatore.txt
+((contatore++))
 
+git commit -m "main_C$i"
+done
+
+git log --oneline
